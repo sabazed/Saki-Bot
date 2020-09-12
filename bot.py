06d1e9b9ab2a9b8af6b8d731f949bb748fe92c7b
sabazed <71182016+sabazed@users.noreply.github.com>
2020-09-12 18:27:08 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+import os
+import random
+from discord.ext import commands, tasks
+
+client = commands.Bot(command_prefix='s/')
+client.remove_command('help')
+status = ['h3ll0fr1end.wav', 'd3bug.mkv', 'da3m0ns.mp4', '3xpl0its.wmv', 'k3rnel-pan1c.ksd', 'logic-b0mb.hc',
+          'm4ster-s1ave.aes', 'h4ndshake.sme', 'succ3ss0r.p12', 'init_5.fve', 'h1dden-pr0cess.axx', 'runtime-error.r00', 'shutdown -r']
+
+
+@client.event
+async def on_ready():
+    print('Bot is online')
+    change_status.start()
+
+
+@tasks.loop(seconds=300)
+async def change_status():
+    await client.change_presence(activity=discord.Game(random.choice(status)))
+
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+
+@client.command(pass_context=True)
+async def help(ctx):
+    autor = ctx.message.author
+    embed = discord.Embed(
+        title='**SakiMeister Help Board**',
+        description='**User Commands:**\ns/mine [size] - generates a minesweeper board, default size 8x8\ns/rps - Play \'Rock Paper Scissors\' against the bot\ns/random [X][Y] - Generate a random number between X and Y\n s/gayrate [member] - Rates how much gay the user is\ns/simprate [member] - Rates how much simpe the user is\ns/dicksize [member] - Measures the dick size of the user'
+    )
+    await ctx.send(embed=embed)
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+token = 'NzUzNTk0ODUyODUxMzE4ODI1.X1od5g.yCFFNzJ2CeuIkO7DSLnUR_ZhVPc'
+
+client.run(token)
---
+++ cogs/Admin.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if ctx.message.author.guild_permissions.value == 2147483647:
+            await ctx.send('Invalid Argument(s)')
+        else:
+            pass
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def clear(self, ctx, amount=1):
+        await ctx.purge(limit=amount)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def kick(self, ctx, user: discord.Member, *, reason=None):
+        await user.kick(reason=reason)
+        await ctx.send(f"{user} have been kicked sucessfully")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def ban(self, ctx, user: discord.Member, *, reason=None):
+        await user.ban(reason=reason)
+        await ctx.send(f"{user} have been bannned sucessfully")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f"{user} have been unbanned sucessfully")
+            return
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+
+
+def setup(client):
+    client.add_cog(Admin(client))
