b6b5846d8955ebbbf6cc31480d9382a02cb9cf4f
sabazed <71182016+sabazed@users.noreply.github.com>
2020-09-29 01:18:00 +0400

    Update joke.py

--- cogs/joke.py
+++ cogs/joke.py
@@ -1,9 +1,6 @@
 import discord
 import joke_generator
-# import random
-# import requests
 
-# from bs4 import BeautifulSoup
 from discord.ext import commands
 
 class Fun(commands.Cog):
@@ -18,13 +15,6 @@ class Fun(commands.Cog):
     @commands.command()
     async def joke(self, ctx):
         await ctx.send(joke_generator.generate())
-
-    # @commands.command(aliases=['jk'])
-    # async def joke(self, ctx):
-    #     source = requests.get('https://generatorfun.com/joke-generator').text
-    #     soup = BeautifulSoup(source, 'lxml')
-    #     embed = discord.Embed(title = soup.find('div', id='gencount').h2.text[:-6],
-    #                           description = soup.find('div', id='gencount').p.text)
 
     @joke.error
     async def jokeerror(self, ctx, error):
